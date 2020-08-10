1042de260f3e321bd67ce687fdede6e5b23db3ae
Yusuf Atakul <66416827+mrjoseph35@users.noreply.github.com>
2020-08-10 20:09:57 +0300

    Add files via upload

---
+++ OCA-LESS3/P114_S16.java
@@ -0,0 +1,54 @@
+public class P114_S16 {
+    public static void main(String[] args) {
+
+
+//    	short s1=200;
+//    	int s2=400;
+//    	long s3=s1+s2;
+//		System.out.println(s3);
+
+		Short s1=200;
+		Integer s2= 400;
+		Long s3= (long) s1+s2;
+
+
+      //  String s4= (String)(s3*s2);   //compilation fails n2
+     //  System.out.println("Sum is "+ s4);
+
+
+
+//		StringBuilder sBuilder= new StringBuilder();
+//		sBuilder.append(4.5);
+//		sBuilder.deleteCharAt(0);
+//		System.out.println(sBuilder);
+//
+        // int, short olursa otomatik longa donusuyor
+        //Integer, Short castingle Long'a donusuyor
+
+
+//
+//        Short s1=200;
+//        Integer s2= 400;
+//        //String s3= (String) s1+s2;  //compilation fails n1
+//
+//        Long s4= (long)(s1+s2);
+//        System.out.println("Sum is "+ s4);
+
+
+//		//Autoboxing
+//		int data=20;
+//		Integer i=new Integer(data);
+//		//i.
+//		System.out.println(i);
+//
+//
+//		//Unboxing
+//		Integer j=new Integer(50);
+//		int a=j.intValue();
+//		//a.
+//		System.out.println(a);
+//
+//		// default ne demek
+
+    }
+}
---
+++ OCA-LESS3/P140_S15.java
@@ -0,0 +1,49 @@
+public class P140_S15 {
+    public static void main(String[] args) {
+//        int wd=0;
+//        String days[]={"sun","mon","wed","sat"};
+//        for(String s:days){
+//            switch (s) {
+//                case "sat":  // wd = 4
+//                case "sun":
+//                    wd-=1;  // wd=-1      //  "sat" icin wd=3  oluyor ve break oldugu icin switch bitiyor.
+//                    break;
+//                case "mon":
+//                    wd++;  // wd=0, break olmadigi icin kod devam edecek
+//                case "wed":
+//                    wd+=2;  // wd=2  switch "mon" icin  bitiyor
+//                            // "wed" icin wd=4 oluyor ve switch "wed" icin  bitiyor
+//
+//            }
+//        }
+//        System.out.println(wd);
+
+
+
+
+//
+//        String dog="Kont";
+//
+//        switch (dog){
+//            case "Karabas":
+//                System.out.println("bizim kopegimiz");
+//
+//            case "Layka":
+//                System.out.println("sizin kopeginiz");
+//
+//            case "Kont":
+//                System.out.println("onlarin kopegi");
+//              //  break;
+//
+//            default:
+//                System.out.println("bu kopegi tanimiyoruz");
+//            case "aaa":
+//                    System.out.println(" bu ne");
+//        }
+//
+//
+//
+//
+
+    }
+}
